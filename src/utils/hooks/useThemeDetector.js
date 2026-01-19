c67d8472ab82123f5abb37c18d8e777dcfffd1c5
Ranjana Yadav <67220186+Ranjana993@users.noreply.github.com>
2026-01-19 11:00:03 +0530

    Remove unused commented code in useThemeDetector
    Removed commented-out implementation of useThemeDetector.

--- src/utils/hooks/useThemeDetector.js
+++ src/utils/hooks/useThemeDetector.js
@@ -35,19 +35,4 @@ const useThemeDetector = () => {
 
 export default useThemeDetector;
 
-/*
-const useThemeDetector = () => {
-    const getCurrentTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches;
-    const [isDarkTheme, setIsDarkTheme] = useState(getCurrentTheme());  
-    const mqListener = (e => {
-        setIsDarkTheme(e.matches);
-    });
-    
-    useEffect(() => {
-      const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-      darkThemeMq.addListener(mqListener);
-      return () => darkThemeMq.removeListener(mqListener);
-    }, []);
-    return isDarkTheme;
-}
-*/
+
